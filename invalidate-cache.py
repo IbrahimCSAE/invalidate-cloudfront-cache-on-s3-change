47b7e3b23622b156455d89c07fcbc85c459ea9fd
Ibrahim <93064150+IbrahimCSAE@users.noreply.github.com>
2022-03-31 20:45:56 -0230

    Update invalidate-cache.py

--- invalidate-cache.py
+++ invalidate-cache.py
@@ -1,12 +1,15 @@
+
 from __future__ import print_function
 
 import boto3
 import time
+import json
+import logging
 
 def lambda_handler(event, context):
     path = ["/*"]
     client = boto3.client('cloudfront')
-    invalidation = client.create_invalidation(DistributionId='Your-Own-CloudFront-Distribution-ID',
+    invalidation = client.create_invalidation(DistributionId='E33J1W1JUL5EDB',
         InvalidationBatch={
             'Paths': {
                 'Quantity': 1,
@@ -14,3 +17,28 @@ def lambda_handler(event, context):
         },
         'CallerReference': str(time.time())
     })
+    invalidation_id = invalidation['Invalidation']['Id']
+    
+    print("Invalidation created successfully with Id: " + invalidation_id)
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.debug(json.dumps(event))
+ 
+    codepipeline = boto3.client('codepipeline')
+    job_id = event['CodePipeline.job']['id']
+ 
+    try:
+        logger.info('Success!')
+        response = codepipeline.put_job_success_result(jobId=job_id)
+        logger.debug(response)
+    except Exception as error:
+        logger.exception(error)
+        response = codepipeline.put_job_failure_result(
+            jobId=job_id,
+            failureDetails={
+              'type': 'JobFailed',
+              'message': f'{error.__class__.__name__}: {str(error)}'
+            }
+        )
+        logger.debug(response)
